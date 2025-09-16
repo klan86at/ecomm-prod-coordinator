bb071d79d19db7c6bf4666292e20883135e4ca3d
klan86at <klang86at@gmail.com>
2025-09-16 14:17:35 +0300

    Project ecommerce initialization done

---
+++ get_lib_version.py
@@ -0,0 +1,15 @@
+import importlib.metadata
+packages = [
+    "langchain",
+    "langchain-core",
+    "python-dotenv"
+]
+
+for pkg in packages:
+    try:
+        version = importlib.metadata.version(pkg)
+        print(f"{pkg}=={version}")
+    except importlib.metadata.PackageNotFoundError:
+        print(f"{pkg} is not installed.")
+
+    
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from ecomm-prod-coordinator!")
+
+
+if __name__ == "__main__":
+    main()
