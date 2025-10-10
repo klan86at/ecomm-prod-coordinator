c9e30ae57e941c1c28995b29b2da0170187c20e4
klan86at <klang86at@gmail.com>
2025-10-10 09:38:02 +0300

    model_loader updated

--- prod_assistant/utils/model_loader.py
+++ prod_assistant/utils/model_loader.py
@@ -9,7 +9,8 @@ from langchain_groq import ChatGroq
 from logger import GLOBAL_LOGGER as log
 from exception.custom_exception import ProductAssistantException
 import asyncio
-
+# 👇 Load .env file BEFORE accessing os.getenv()
+load_dotenv()  # This populates os.environ with values from .env
 
 class ApiKeyManager:
     """
